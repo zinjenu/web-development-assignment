a59332214e50b0bba7eb26de1c8a8f7974200123
zinjenu <zinnyjenny.ue@gmail.com>
2023-11-23 18:58:59 +0100

    added to index.html

---
+++ index.js
@@ -0,0 +1,18 @@
+const day = "Monday";
+switch (day) {
+case "Monday":
+    console.log ("Day is Monday ");
+    break;
+case "Tuesday":
+    console.log("Day is Tuesday");
+    break;
+case "Wednesday":
+    console.log("Day is Wednesday");
+    break;
+case "Thursday":
+    console.log("Day is Thursday");
+    break;
+case "Friday":
+    console.log("Day is Friday");
+    break;                 
+}
